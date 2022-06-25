2409cdbc010ec825323721a1b639865a12d6e444
Aaron Chris <rooneychris83@gmail.com>
2022-06-25 10:42:34 +0100

    Done

--- spliceslice.js
+++ spliceslice.js
@@ -1,5 +1,23 @@
 // splice()
 
 // This method can be used to perform CRUD operations 
+// Returns a deleted Element 
+
 
 // slice()
+// Slices out a piece of an array into a new array
+// slice() method creates a new array
+
+
+
+const mon = ['jan', 'march', 'april', 'june', 'july' ]
+
+
+
+// Update from index 0 to 2 
+console.log(mon.splice(0, 2, 'May', 'August'))
+console.log(mon)
+
+console.log(mon.slice(1, 3))
+
+
