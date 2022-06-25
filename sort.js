65cff4013cb77a1fbe42cc3dd6a3626ebad1bde3
Aaron Chris <rooneychris83@gmail.com>
2022-06-25 09:22:24 +0100

    added sorting methods

--- sort.js
+++ sort.js
@@ -1,5 +1,14 @@
-const month = ['jan',' march', 'april', 'june', 'july' ]
-const nums = [354, 6767, 2344, 12, 455,676, 78]
+const months = ['jan', 'march', 'april', 'june', 'july' ]
+const num = [354, 6767, 2344, 12, 455,676, 78]
 
 
 
+// Sorts the elements in the array in order 
+console.log(months.sort())
+console.log(num.sort())
+
+
+
+// For sorting interger element
+
+console.log(num.sort((a, b) => a - b))
