1402643679da66cf37c240dd03133e998ecfa900
Aaron Chris <rooneychris83@gmail.com>
2022-06-25 09:03:53 +0100

    Added pop and push

---
+++ poppingandpushing.js
@@ -0,0 +1,6 @@
+const month = ['jan',' march', 'april', 'june', 'july' ]
+const nums = [354, 6767, 2344, 12, 455,676, 78]
+
+
+console.log(month.push('December')) //adds to the month array and increases the length
+console.log(month)
