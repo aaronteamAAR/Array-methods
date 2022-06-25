2e9a2d4117ef272c1852874d43a701035f8e35ce
Aaron Chris <rooneychris83@gmail.com>
2022-06-25 09:07:11 +0100

    Adds unshift method

--- poppingandpushing.js
+++ poppingandpushing.js
@@ -4,3 +4,8 @@ const nums = [354, 6767, 2344, 12, 455,676, 78]
 
 console.log(month.push('December')) //adds to the month array and increases the length
 console.log(month)
+
+
+// Adds the word to the beginning of the array
+console.log(month.unshift('Feb')) 
+console.log(month)
